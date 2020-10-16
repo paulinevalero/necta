b54cb556b79df5d70c88a6ea009b13483e69f7df
paulinevalero <paulinevalero@outlook.com>
2020-10-16 12:12:06 -0500

    grid left 7 grid right 5

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid'
 import {Button, makeStyles} from '@material-ui/core'
 import image from './image.png'
-
-/* const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-})); */
 
 const useStyles = makeStyles({
   font: {
@@ -28,21 +16,15 @@ function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-
-      </Grid>
-      </div>
-      <Grid container spacing={3} justify="center">
-        <Grid item xs={6} >
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={7}>
           <h1 className={classes.font}>¿Necesitas un proveedor pero no sabes dónde encontrarlo?</h1>
           <p className={classes.font}>Somos la plataforma de licitaciones enfocada en PyMES en México</p>
           <Button variant="contained" color="primary" className={classes.font}>Comenzar un proyecto</Button>
         </Grid>
-        <img src={image} alt="person with computer"/>
+        <Grid item xs={12} md={5}>
+          <img src={image} alt="person with computer"/>
+        </Grid>
       </Grid>
     </React.Fragment>
   );
