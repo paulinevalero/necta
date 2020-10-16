0311b87c0698cf713d146b6cce32c060d5ed4b1c
paulinevalero <paulinevalero@outlook.com>
2020-10-15 22:23:35 -0500

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid'
+import {Button, makeStyles} from '@material-ui/core'
+import image from './image.png'
+
+/* const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+})); */
+
+const useStyles = makeStyles({
+  font: {
+    fontFamily: 'Nunito Sans',
+  }
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+
+      </Grid>
+      </div>
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={6} >
+          <h1 className={classes.font}>¿Necesitas un proveedor pero no sabes dónde encontrarlo?</h1>
+          <p className={classes.font}>Somos la plataforma de licitaciones enfocada en PyMES en México</p>
+          <Button variant="contained" color="primary" className={classes.font}>Comenzar un proyecto</Button>
+        </Grid>
+        <img src={image} alt="person with computer"/>
+      </Grid>
+    </React.Fragment>
   );
 }
 
